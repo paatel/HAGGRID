9c2c7874b7ba956059e5d9f8157560f8f5f3c075
Zach Hofley <zhofley@gmail.com>
2018-02-27 16:17:20 -0500

    Added reviews logic, new review view

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,27 @@
 class ReviewsController < ApplicationController
+  before_action :set_review, only:[:destroy]
+  def new
+    @review = Review.new
+  end
+
+  def create
+    @review = Review.new(review_params)
+    @review.item = @item
+  end
+
+  def destroy
+    @review.destroy
+    redirect_to item_path(@item)
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:rating, :content)
+  end
+
+  def set_review
+    @review = Review.find(params[:id])
+  end
+
 end
