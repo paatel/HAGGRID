9936be11c1074934779475443d03e13705781934
Aayush Patel <patelaa77@gmail.com>
2018-02-28 14:31:46 -0500

    haggle-logic-with-price

--- app/controllers/haggles_controller.rb
+++ app/controllers/haggles_controller.rb
@@ -1,2 +1,8 @@
 class HagglesController < ApplicationController
+  def create
+    if Haggle.where(item: params[:item_id], user: current_user).empty?
+      Haggle.create(user: current_user, item_id: params[:item_id], roll: rand(6)+1)
+      redirect_to item_path(params[:item_id])
+   end
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180227224833) do
+ActiveRecord::Schema.define(version: 20180228190302) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,11 @@ ActiveRecord::Schema.define(version: 20180227224833) do
   create_table "haggles", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "roll"
+    t.bigint "user_id"
+    t.bigint "item_id"
+    t.index ["item_id"], name: "index_haggles_on_item_id"
+    t.index ["user_id"], name: "index_haggles_on_user_id"
   end
 
   create_table "items", force: :cascade do |t|
@@ -63,4 +68,6 @@ ActiveRecord::Schema.define(version: 20180227224833) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "haggles", "items"
+  add_foreign_key "haggles", "users"
 end
