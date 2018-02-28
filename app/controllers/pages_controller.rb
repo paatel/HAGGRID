ffbcb1647d8cc442de3dc2559619e8418f0d1575
Zach Hofley <zhofley@gmail.com>
2018-02-28 11:07:27 -0500

    review now has both buyer and seller and rating is calculated by average

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,6 +3,12 @@ class PagesController < ApplicationController
 
   def profile
     @items = Item.where(:user == current_user)
+    @reviews = Review.where(:seller == current_user)
+    @rating = 0
+    @reviews.each do |review|
+      @rating += review[:rating]
+    end
+    @rating = @rating / (@reviews.length)
   end
 
   def home
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,12 @@
 class Review < ApplicationRecord
   belongs_to :item
-  belongs_to :user
+  belongs_to :buyer,
+             :class_name => "User",
+             :foreign_key => "buyer_id"
+
+  belongs_to :seller,
+             :class_name => "User",
+             :foreign_key => "seller_id"
+
   validates :rating, presence: true
 end
