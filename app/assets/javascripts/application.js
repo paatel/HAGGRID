6fb6bc137066787af9c18cd5aa60b3621b535d18
Zach Hofley <zhofley@gmail.com>
2018-03-02 11:44:45 -0500

    added rating stars

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,3 +1,5 @@
 //= require rails-ujs
+//= require jquery
 //= require bootstrap-sprockets
+//= require raty
 //= require_tree
---
+++ app/assets/javascripts/ratings.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function(){
+  $('#rating').raty({
+    path: '/assets/ratings'
+  });
+});
